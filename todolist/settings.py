f758c35c9d7ae4af06a501da36075b34536ea434
nickwild77 <knezhevich77@gmail.com>
2021-12-18 15:11:41 +0300

    ViewSets для юзеров, проектов и задач. Добавлена фильрация и пагинация.

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'django_filters',
 
     'users',
     'TODO',
@@ -147,4 +148,11 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth import get_user_model
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from users.serializers import UserSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+# class UserModelViewSet(ModelViewSet):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+class CustomUserModelViewSet(ListModelMixin, RetrieveModelMixin,
+                             UpdateModelMixin, GenericViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
