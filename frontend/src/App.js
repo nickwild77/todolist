135f0862c7c7ff6137590ea68e23408454dadd2c
nickwild77 <knezhevich77@gmail.com>
2022-01-07 04:44:48 +0300

    login and logout form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
+import {HashRouter, BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
+import axios from "axios";
 import './App.css';
-import UserList from './components/User.js';
-import Menu from "./components/Menu";
-import Footer from "./components/Footer";
-import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
+
+import UserList from "./components/User";
+import LoginForm from "./components/LoginForm";
 import ToDosList from "./components/ToDos";
 import ProjectsList, {ProjectDetail} from "./components/Projects";
 
 const apiUrl = 'http://127.0.0.1:8000/api/';
 
-const page404 = ({path}) => {
+const NotFound = () => {
+    const location = useLocation()
     return (
-        <h1>Page '{path.pathname}' not found</h1>
+        <div>Page {location.pathname} not found</div>
     )
-}
+};
 
 class App extends React.Component {
     constructor(props) {
@@ -28,7 +30,33 @@ class App extends React.Component {
             previousToDosUrl: null,
             nextToDosUrl: null,
             toDoList: [],
+            'token': '',
         };
+    }
+
+    get_token(login, password) {
+        axios
+            .post('http://127.0.0.1:8000/api-token-auth/', {"username": login, "password": password})
+            .then(response => {
+                const token = response.data.token
+                console.log(token)
+                localStorage.setItem('token', token)
+                this.setState({
+                    'token': token
+                })
+            })
+            .catch(error => console.log(error))
+    }
+
+    logout() {
+        localStorage.setItem('token', '')
+        this.setState({
+            'token': ''
+        })
+    }
+
+    is_auth() {
+        return !!this.state.token
     }
 
     loadUsers(url) {
@@ -81,9 +109,9 @@ class App extends React.Component {
                         toDoList: toDos,
                     }
                 );
-            })
-            .catch(error => console.log(error));
+            }).catch(error => console.log(error));
     };
+
 
     loadProjects(url) {
         fetch(`${url}`,
@@ -113,51 +141,46 @@ class App extends React.Component {
     };
 
     componentDidMount() {
+        const token = localStorage.getItem('token')
         this.loadUsers(`${apiUrl}users/`);
         this.loadToDo(`${apiUrl}todo/`);
         this.loadProjects(`${apiUrl}projects/`);
+        this.setState({
+            'token': token
+        })
     }
 
     render() {
         return (
-            <div className={'App'}>
+            <div>
                 <HashRouter>
-                    <Menu/>
-                    <Switch>
-                        <Route exact path={'/'}>
-                            <UserList users={this.state.userList}
-                                      nextPage={this.state.nextUsersUrl}
-                                      previousPage={this.state.previousUsersUrl}
-                                      load={this.loadUsers.bind(this)}
-                            />
-                        </Route>
-                        <Route exact path={'/todos/'}>
-                            <ToDosList toDosList={this.state.toDoList}
-                                       nextPage={this.state.nextToDosUrl}
-                                       previousPage={this.state.previousToDosUrl}
-                                       load={this.loadToDo.bind(this)}
-                            />
-                        </Route>
-                        <Route exact path={'/projects/'}>
-                            <ProjectsList projectsList={this.state.projectsList}
-                                          nextPage={this.state.nextProjectsUrl}
-                                          previousPage={this.state.previousProjectsUrl}
-                                          load={this.loadProjects.bind(this)}
-                            />
-                        </Route>
-                        <Route exact path={'/projects/:id/'}>
-                            <ProjectDetail projectsList={this.state.projectsList}/>
-                        </Route>
-                        <Redirect from={'/users/'} to={'/'}/>
-                        <Route component={page404}/>
-                    </Switch>
-                    <Footer/>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Пользователи</Link></li>
+                            <li><Link to="/projects">Проекты</Link></li>
+                            <li><Link to="/todos">Задачи</Link></li>
+                            <li>
+                                {this.is_auth() ? <button onClick={() => this.logout()}> Выйти </button> :
+                                    <Link to="/login">Войти</Link>}
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route exact path='/' element={<UserList users={this.state.userList}/>}/>
+                        <Route exact path='/projects' element={<ProjectsList projectsList={this.state.projectsList}/>}/>
+                        <Route exact path='/todos' element={<ToDosList toDosList={this.state.toDoList}/>}/>
+                        <Route exact path='/login'
+                               element={<LoginForm
+                                   get_token={(login, password) => this.get_token(login, password)}/>}/>
+                        <Route path="/users" element={<Navigate to="/"/>}/>
+                        <Route path='/projects/:id'
+                               element={<ProjectDetail projectsList={this.state.projectsList}/>}/>
+                        <Route path="*" element={<NotFound/>}/>
+                    </Routes>
                 </HashRouter>
             </div>
-        );
+        )
     }
-
 }
-
 
 export default App;
