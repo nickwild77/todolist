748c92800b2ba0518a95ecc22e2e47827844f043
nickwild77 <knezhevich77@gmail.com>
2022-01-29 21:36:30 +0300

    API users version 2, swagger documentation

--- todolist/settings.py
+++ todolist/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'frontend' / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+        BASE_DIR / 'frontend' / 'build' / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
